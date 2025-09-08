a99ee5782301b68e3374383d70257349b550cc38
Nikoline Burman <nikoline.burman@gmail.com>
2025-09-08 20:50:50 +0200

    Finish Basics 3 - Mathematical Surfaces

--- Week01 - Basics 2 Graph/Assets/Scripts/Graph.cs
+++ Week01 - Basics 2 Graph/Assets/Scripts/Graph.cs
@@ -8,33 +8,39 @@ public class Graph : MonoBehaviour
 
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
     Transform[] points;
 
     void Awake()
     {
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
-        Vector3 position = Vector3.zero;
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
     }
 
-    void Update()
-    { 
-        float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+	void Update () {
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+		float time = Time.time;
+		float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f; 
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
-    }
+	}
 }
